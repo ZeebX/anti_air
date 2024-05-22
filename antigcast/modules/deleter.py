f228d4e46f453426fb64995c32a1b8219032e64d
ZeebFly <163663744+ZeebX@users.noreply.github.com>
2024-05-23 02:28:02 +0700

    Update deleter.py

--- antigcast/modules/deleter.py
+++ antigcast/modules/deleter.py
@@ -12,43 +12,43 @@ from antigcast.helpers.message import *
 from antigcast.helpers.database import *
 
 
-@Bot.on_message(filters.command("bl") & ~filters.private & Admin)
+@Bot.on_message(filters.command("remove") & ~filters.private & Admin)
 async def addblmessag(app : Bot, message : Message):
     trigger = get_arg(message)
     if message.reply_to_message:
         trigger = message.reply_to_message.text or message.reply_to_message.caption
 
-    xxnx = await message.reply(f"`Menambahakan` {trigger} `ke dalam blacklist..`")
+    xxnx = await message.reply(f"`Message` {trigger} `added to blacklist`")
     try:
         await add_bl_word(trigger.lower())
     except BaseException as e:
         return await xxnx.edit(f"Error : `{e}`")
 
     try:
-        await xxnx.edit(f"{trigger} `berhasil di tambahkan ke dalam blacklist..`")
+        await xxnx.edit(f"{trigger} `Successfully added to the list`")
     except:
-        await app.send_message(message.chat.id, f"{trigger} `berhasil di tambahkan ke dalam blacklist..`")
+        await app.send_message(message.chat.id, f"{trigger} `Successfully added to the list`")
 
     await asyncio.sleep(5)
     await xxnx.delete()
     await message.delete()
 
-@Bot.on_message(filters.command("unbl") & ~filters.private & Admin)
+@Bot.on_message(filters.command("delmsg") & ~filters.private & Admin)
 async def deldblmessag(app : Bot, message : Message):
     trigger = get_arg(message)
     if message.reply_to_message:
         trigger = message.reply_to_message.text or message.reply_to_message.caption
 
-    xxnx = await message.reply(f"`Menghapus` {trigger} `ke dalam blacklist..`")
+    xxnx = await message.reply(f"`Delete` {trigger} `the list`")
     try:
         await remove_bl_word(trigger.lower())
     except BaseException as e:
         return await xxnx.edit(f"Error : `{e}`")
 
     try:
-        await xxnx.edit(f"{trigger} `berhasil di hapus dari blacklist..`")
+        await xxnx.edit(f"{trigger} `Successfully removed from the list`")
     except:
-        await app.send_message(message.chat.id, f"{trigger} `berhasil di hapus dari blacklist..`")
+        await app.send_message(message.chat.id, f"{trigger} `Successfully removed from the list`")
 
     await asyncio.sleep(5)
     await xxnx.delete()
@@ -57,7 +57,7 @@ async def deldblmessag(app : Bot, message : Message):
 
 @Bot.on_message(filters.text & ~filters.private & Member & Gcast)
 async def deletermessag(app : Bot, message : Message):
-    text = f"Maaf, Grup ini tidak terdaftar di dalam list. Silahkan hubungi @Usern4meDoesNotExist404 Untuk mendaftarkan Group Anda.**"
+    text = f"***Sorry, the group is not registered, contact @zeebdisini to register your group**"
     chat = message.chat.id
     chats = await get_actived_chats()
     if chat not in chats:
