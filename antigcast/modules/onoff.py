bfed310de6bb69ccff6982b47fcdf5b13617fee4
ZeebFly <163663744+ZeebX@users.noreply.github.com>
2024-05-23 01:58:20 +0700

    Update onoff.py

--- antigcast/modules/onoff.py
+++ antigcast/modules/onoff.py
@@ -14,21 +14,20 @@ from antigcast.helpers.database import *
 from antigcast.helpers.database import admin_filter
 
 
-# BY DANTE 
-@Bot.on_message(filters.command(["on"]) & admin_filter)
+@Bot.on_message(filters.command(["antigcast on"]) & admin_filter)
 async def antigcaston(app : Bot, message : Message):
     chat_id = message.chat.id
     chat_name = message.chat.title
     hari = get_arg(message)
     if not hari:
         hari = "30"
-    xxnx = await message.reply(f"`Anti gcast berhasil di aktifkan!`")
+    xxnx = await message.reply(f"`Anti Gcast berhasil di aktifkan!`")
     now = datetime.datetime.now(timezone("Asia/Jakarta"))
     expired = now + relativedelta(days=int(hari))
     expired_date = expired.strftime("%d-%m-%Y")
     chats = await get_actived_chats()
     if chat_id in chats:
-        msg = await message.reply("Silahkan hidupkan anti gcast /on agar bot bisa digunakan.")
+        msg = await message.reply("Type /antigcast on")
         await asyncio.sleep(10)
         await msg.delete()
         return
@@ -40,19 +39,19 @@ async def antigcaston(app : Bot, message : Message):
     except BaseException as e:
         print(e)
 
-    await xxnx.edit(f"Anti broadcast on di group : `{chat_name}`")
+    await xxnx.edit(f"Anti Gcast on in group : \n`{chat_name}`")
     await asyncio.sleep(10)
     await xxnx.delete()
     await message.delete()
 
-@Bot.on_message(filters.command(["off"]) & admin_filter)
+@Bot.on_message(filters.command(["antigcast off"]) & admin_filter)
 async def antigcastoff(app : Bot, message : Message):
     chat_id = get_arg(message)
 
     if not chat_id:
         chat_id = message.chat.id
         
-    xxnx = await message.reply(f"`Anti broadcast Berhasil di nonaktifkan`")
+    xxnx = await message.reply(f"`Anti Gcast berhasil di nonaktifkan!`")
     try:
         await rem_actived_chat(chat_id)
         await rem_expired_date(chat_id)
