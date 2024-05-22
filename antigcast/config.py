684c1502202705b8be2b2c84d151316d1ccd1066
ZeebFly <163663744+ZeebX@users.noreply.github.com>
2024-05-23 02:38:52 +0700

    Update config.py

--- antigcast/config.py
+++ antigcast/config.py
@@ -2,22 +2,22 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6936605005:AAGylSU8pF84dUMzMXm0QpmyXHcFkbzPrco")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7176317726:AAEw6u5eAFot7PeZk44i8DZ0D0cRAVRoMd8")
 BOT_WORKERS = int(os.environ.get("BOT_WORKERS", "4"))
 
 APP_ID = int(os.environ.get("APP_ID", "26724473"))
 API_HASH = os.environ.get("API_HASH", "7bc7d1f9b2f3d3f1bfd272db56ac0ba1")
 
-LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002009684047"))
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002044478374"))
 
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://Xyxx:Helmi4636@cluster0.9yy5ve9.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DB_NAME", "Xyxx")
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://0852:0852@cluster0.7ucs9c3.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DB_NAME", "CleanZb")
 BROADCAST_AS_COPY = True
 
 PLUG = dict(root="antigcast/plugins")
 
-OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "940232666").split())]
-OWNER_NAME = os.environ.get("OWNER_NAME", "Dante")
+OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "1325957770").split())]
+OWNER_NAME = os.environ.get("OWNER_NAME", "Zeeb")
 
 
 LOG_FILE_NAME = "antigcast_logs.txt"
@@ -36,6 +36,6 @@ def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
 
 CREATOR = [
-    940232666, # p
+    1325957770, # p
 ]
 
